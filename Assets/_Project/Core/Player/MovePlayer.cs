9f9f84bb943c7064af17fccaa05e194530e5e540
ArtemVoinPeruna <artemlavrentev578@gmail.com>
2024-09-21 11:24:28 +0300

    добавлен скрипт, пока пустой '_'

--- Assets/_Project/Core/Player/MovePlayer.cs
+++ Assets/_Project/Core/Player/MovePlayer.cs
@@ -2,17 +2,10 @@ using UnityEngine;
 
 namespace Core.Player
 {
-    [RequireComponent(typeof(Rigidbody2D))]
     public class MovePlayer : MonoBehaviour
     {
         public float speed = 5f;
-        private Rigidbody2D rb;
         private Vector2 movement;
-
-        void Start()
-        {
-            rb = GetComponent<Rigidbody2D>();
-        }
 
         void Update()
         {
@@ -20,16 +13,13 @@ namespace Core.Player
             float moveY = Input.GetAxis("Vertical");
 
             movement = new Vector2(moveX, moveY);
-        }
 
-        void FixedUpdate()
-        {
             MovePlayerCharacter();
         }
 
         private void MovePlayerCharacter()
         {
-            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+            transform.position += (Vector3)movement * speed * Time.deltaTime;
         }
     }
 }
---
+++ Assets/_Project/Core/Player/UiConteiner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Core.Player
+{
+    public class UiConteiner : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
