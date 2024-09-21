f911c9dcadc41294e2fcf8567bef7aa4867b37bf
pvpmastero4ek9 <157130918+pvpmastero4ek9@users.noreply.github.com>
2024-09-21 12:00:01 +0300

    продолжил писать логику спавна врагов

--- Assets/_Project/Core/Enemys/SpawnEnemy.cs
+++ Assets/_Project/Core/Enemys/SpawnEnemy.cs
@@ -10,6 +10,7 @@ namespace Core.Enemys
     {
         [field: SerializeField] private List<Entity> _enemies;
         [field: SerializeField] private List<Transform> _spawnPoints;
+
         private LvlStage _lvlStage = new();
         
         public void SpawnRandomEnemy()
@@ -22,7 +23,23 @@ namespace Core.Enemys
 
             Transform spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Count)];
 
-            
+            int MinDamageEnemy = _enemies.Min(x => x.Damage);
+
+            List<Entity> MinDamageEnemys = _enemies.Where(x => x.Damage <= MinDamageEnemy).ToList();
+            List<Entity> MaxDamageEnemys = _enemies.Where(x => x.Damage > MinDamageEnemy * 0.2).ToList();
+
+            for (var i = 0; i < _enemies.Count; i++)
+            { 
+                if (ChanceReturn(MinDamageEnemy) == 1)
+                {
+                    GameObject fg = Instantiate(MinDamageEnemys[Random.Range(0, MinDamageEnemys.Count)].Enemy_PREFAB); //тут надо поменять GameObject на тип, который будет инфу по UI распредеять
+                }
+                else
+                {
+                    
+                }
+            }
+
         }
 
         private void GetEnemyByPriority()
