61b830df6aa47ebfe732041699f9bfce764bae06
ArtemVoinPeruna <artemlavrentev578@gmail.com>
2024-09-26 21:20:40 +0300

    Изменения (Небольшие)

--- Assets/_Project/Core/Player/UiConteiner.cs
+++ Assets/_Project/Core/Player/UiConteiner.cs
@@ -1,17 +1,28 @@
+using Entitys;
 using UnityEngine;
 
 namespace Core.Player
 {
     public class UiConteiner : MonoBehaviour
     {
-        void Start()
-        {
-        
-        }
+        [field: SerializeField] public Entity SpecPl { get; set; }
 
-        void Update()
+        public delegate void BarChangedDelegate(Entity bar);
+        public event BarChangedDelegate BarChanged;
+
+        public UiConteiner Init(Entity bar)
         {
-        
+            if (bar != null)
+            {
+                SpecPl = bar;
+                BarChanged?.Invoke(bar);
+            }
+            else
+            {
+                Debug.LogError("Attempted to initialize with a null RewardLine.");
+            }
+
+            return this;
         }
     }
 }
---
+++ Assets/_Project/UI/Player/HealthBar.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Entitys;
+
+
+namespace Ui.Player
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [field: SerializeField] private Entity _entityPlayer;
+
+
+        private void Update()
+        {
+            
+        }
+
+    }
+}
