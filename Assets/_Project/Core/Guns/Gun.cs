94bc70d9c6d188252394ae4af9d1e8ddad4731a9
PetrochenkoPetr <pet.13petr@gmail.com>
2024-09-28 09:21:33 +0300

    сделал скриптейбле обджекты оружия и леона на главный экран

--- Assets/_Project/Core/Guns/Gun.cs
+++ Assets/_Project/Core/Guns/Gun.cs
@@ -9,15 +9,14 @@ namespace Core.Guns
         public class Entity : ScriptableObject
         {
             [field: SerializeField] public int Damage { get; private set; }
-            [field: SerializeField] public int Reload { get; private set; }
+            [field: SerializeField] public int AttackSpeed { get; private set; }
             [field: SerializeField] public int Speed { get; private set; }            
             [field: SerializeField] public Sprite GunSprite { get; private set; }
-
-            public void GunChange (int _ammo, int _attackspeed,int _damage)
+            public void GunChange (int _speed, int _attackSpeed,int _damage)
             {
-            // Ammo
-            // AttackSpeed = _Attackspeed;
                 Damage = _damage;
+                AttackSpeed = _attackSpeed;
+                Speed = _speed;
             }
         }
     }
